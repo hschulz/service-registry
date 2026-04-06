9f8c844974c5ef648c945fdf14b56d4091fcf06d
Hauke Schulz <hauke27@gmail.com>
2026-04-06 11:19:07 +0200

    chore: add dist/ to .gitignore and remove tracked build artifacts

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { AbstractService } from "./src/AbstractService";
-import { ServiceInterface } from "./src/ServiceInterface";
-import { ServiceRegistry } from "./src/ServiceRegistry";
-import { ServiceState } from "./src/ServiceState";
-export { AbstractService, ServiceInterface, ServiceRegistry, ServiceState };
-//# sourceMappingURL=index.d.ts.map
